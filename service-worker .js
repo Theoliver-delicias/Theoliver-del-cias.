f910222800c1914097f98a7fd4f631894367ff74
Theoliver-delícias <emanueloliveira411@gmail.com>
2025-09-19 19:25:37 -0300

    Add files via upload

--- service-worker .js
+++ service-worker .js
@@ -1,18 +1,27 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("theoliver-cache").then(cache => {
-      return cache.addAll([
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
+const CACHE_NAME = 'theoliver-cache-v1';
+const TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  // icons will be cached on first fetch
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request).then(r=>{
+      // optionally cache new requests (images, icons)
+      if (evt.request.method === 'GET' && r && r.status === 200){
+        const clone = r.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(evt.request, clone);
+        });
+      }
+      return r;
+    }).catch(()=>caches.match('./index.html')))
   );
 });
